8b2426ffac74fa90fcc389df61b2b7658db24ec8
Adrian Krynicki <adrian.krynicki88@gmail.com>
2025-01-31 11:27:42 +0100

    Refactor Chicken class with service architecture and animation improvements
    - Implemented service-based dependency injection for better modularity
    - Integrated with AnimationManager for centralized animation control
    - Removed interval-based animation in favor of AnimationManager
    - Added precise collision offset properties
    - Simplified chicken movement and death animation logic
    - Improved code organization and reduced complexity
    - Removed manual coordinate mapping in favor of direct spawn points
    - Enhanced performance by eliminating interval checks
    - Added isKilled state for better death handling
    - Updated chicken initialization with services parameter

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,12 +1,20 @@
 class Chicken extends MovableObject {
-  y = 350;
+  y = 340;
   height = 80;
   width = 70;
   hp = 5;
+
+  animation = null;
   isAnimated = false;
-  isDeathAnimationPlaying = false;
-  walkInterval = null;
-  deathCheckInterval = null;
+  isKilld = false;
+
+  // Kollisions-Offset für präzisere Hitboxen
+  offset = {
+    top: 10,
+    left: 5,
+    right: 5,
+    bottom: 0,
+  };
   IMAGES_WALKING = [
     "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
     "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
@@ -14,84 +22,55 @@ class Chicken extends MovableObject {
   ];
   IMAGE_DEAD = "img/3_enemies_chicken/chicken_normal/2_dead/dead.png";
 
-  constructor(id) {
+  constructor(id, x, services) {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.loadImages(this.IMAGES_WALKING);
     this.number = id;
-    this.x = this.getCoordinates(id);
-    this.speed = 5 + Math.random() * 2;
+    this.speed = 70 + Math.random() * 2;
+    this.x = x;
+    this.services = services;
   }
 
-  getCoordinates(id) {
-    let coordinates = [725, 1400, 1500, 2200, 2300, 2400, 3000, 3300, 3400, 3500, 4000, 4100, 4300, 4700, 4800];
-    let index = id - 1;
-    let startPosition;
+  initializeAnimation() {
+    if (!this.isAnimated) {
+      const animation = {
+        update: (deltaTime) => {
+          if (!this.services?.world?.gameState?.gamePaused && !this.isDead()) {
+            this.animateChicken(deltaTime);
+          }
+        },
+      };
 
-    if (index >= 0 && index < coordinates.length) {
-      startPosition = coordinates[index];
+      this.services.animationManager.addAnimation(animation);
+      this.isAnimated = true;
+    }
+  }
+
+  removeAnimation(animationManager) {
+    if (this.animation) {
+      animationManager.removeAnimation(this.animation);
+      this.animation = null;
+      this.isAnimated = false;
+      this.isKilld = true;
+    }
+  }
+
+  animateChicken(deltaTime) {
+    if (!this.isDead()) {
+      this.playAnimation(this.IMAGES_WALKING, deltaTime, 10);
+      this.moveLeft(deltaTime);
+      this.otherDirection = false;
     } else {
-      startPosition = 300;
-    }
-
-    return startPosition;
-  }
-
-  startAnimation() {
-    if (this.walkInterval) {
-      clearInterval(this.walkInterval);
-    }
-    if (this.deathCheckInterval) {
-      clearInterval(this.deathCheckInterval);
-    }
-
-    this.walkInterval = setInterval(() => {
-      if (this.isAnimated && !this.isDeathAnimationPlaying) {
-        this.playAnimation(this.IMAGES_WALKING);
-        this.moveLeft();
-        this.OtherDirection = false;
-      }
-    }, 100);
-
-    this.deathCheckInterval = setInterval(() => {
-      if (this.isDead() && !this.isDeathAnimationPlaying) {
-        clearInterval(this.walkInterval);
-        this.playDeathAnimation();
-      }
-    }, 500);
-  }
-
-  stopAnimation() {
-    if (this.walkInterval) {
-      clearInterval(this.walkInterval);
-      this.walkInterval = null;
-    }
-    if (this.deathCheckInterval) {
-      clearInterval(this.deathCheckInterval);
-      this.deathCheckInterval = null;
+      this.playDeathAnimation();
     }
   }
 
   playDeathAnimation() {
-    return new Promise(resolve => {
-      this.isDeathAnimationPlaying = true;
-      this.loadImage(this.IMAGE_DEAD);
-      setTimeout(() => {
-        this.isDeathAnimationPlaying = false;
-        resolve();
-      }, 200);
-    });
-  }
-
-  deadChickenSound() {
-    world.sounds.playAudio("chicken");  
-  }
-
-  getHitbox() {
-    return {
-      x: this.x + this.width * 0.15,  // 15% vom Rand
-      y: this.y + this.height * 0.1,
-      width: this.width * 0.7,   // 70% der Originalbreite
-      height: this.height * 0.8
-    };
+    this.isDeathAnimationPlaying = true;
+    this.loadImage(this.IMAGE_DEAD);
+    setTimeout(() => {
+      this.isDeathAnimationPlaying = false;
+      this.isKilld = true;
+    }, 100);
   }
 }
