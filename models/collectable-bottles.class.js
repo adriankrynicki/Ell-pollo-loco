04a391cb9607f1b43929450f89188c5b4b6a8dd8
Adrian Krynicki <adrian.krynicki88@gmail.com>
2025-01-31 11:30:39 +0100

    Refactor CollectableBottles class with improved collision detection
    - Added precise collision offset properties
    - Expanded coordinates array for more bottle positions
    - Removed direct world dependency for sound handling
    - Replaced getHitbox method with offset object
    - Enhanced code organization and readability
    - Added documentation for collision offset
    - Improved bottle placement throughout the level

--- models/collectable-bottles.class.js
+++ models/collectable-bottles.class.js
@@ -1,40 +1,36 @@
-class CollectableBottles extends DrawableObject {
-    y = 350;
-    height = 90;
-    width = 80;
+class CollectableBottles extends MovableObject {
+  y = 350;
+  height = 90;
+  width = 80;
+  // Kollisions-Offset für präzisere Hitboxen
+  offset = {
+    top: 20,
+    left: 30,
+    right: 30,
+    bottom: 20,
+  };
 
-    constructor(imagePath, bottleCount) {
-      super().loadImage(imagePath);
-  
-      this.x = this.getCoordinates(bottleCount);
+  constructor(imagePath, bottleCount) {
+    super().loadImage(imagePath);
+
+    this.x = this.getCoordinates(bottleCount);
+  }
+
+  getCoordinates(bottleCount) {
+    let coordinates = [
+      720, 1400, 1700, 2800, 3100, 3600, 3900, 4200, 4500, 5000, 5300, 5600,
+      5900, 6200, 6500, 6800, 7100, 7400, 7700, 8000, 8720, 9400, 10000, 11000,
+      12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000,
+    ];
+    let index = bottleCount - 1;
+    let startPosition;
+
+    if (index >= 0 && index < coordinates.length) {
+      startPosition = coordinates[index];
+    } else {
+      startPosition = 700;
     }
-  
-    getCoordinates(bottleCount) {
-      let coordinates = [720, 1400, 1700, 2800, 3100, 3600, 3900, 4200, 4500, 5000];
-      let index = bottleCount - 1;
-      let startPosition;
-  
-      if (index >= 0 && index < coordinates.length) {
-        startPosition = coordinates[index];
-      } else {
-        startPosition = 700;
-      }
-  
-      return startPosition;
-    }
 
-    
-  bottleCollectSound() {
-    world.sounds.playAudio("bottle_collect");
+    return startPosition;
   }
-
-  getHitbox() {
-    return {
-      x: this.x + this.width * 0.25,  // 25% vom Rand
-      y: this.y + this.height * 0.25,
-      width: this.width * 0.5,   // 50% der Originalbreite
-      height: this.height * 0.5
-    };
-  }
-  }
-  
+}
